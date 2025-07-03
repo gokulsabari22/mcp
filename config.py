1ea3d39b6fde35f949df1966a23370005dd966a9
gokul <gokul.gowri.shankar.krishnan@sap.com>
2025-07-03 19:35:38 +0530

    Update configuration and enhance main.py functionality
    - Added config.py to define model and proxy client names
    - Updated main.py to utilize the new proxy client and configuration settings
    - Enhanced .gitignore to exclude __pycache__ directory
    - Updated pyproject.toml to include sap-ai-sdk-gen as a dependency
    - Modified uv.lock to reflect new dependencies and their versions

---
+++ config.py
@@ -0,0 +1,2 @@
+MODEL_NAME = "gpt-4o-mini"
+PROXY_CLIENT_NAME = "gen-ai-hub"
--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
-from langchain_mcp_adapters import load_mcp_tools
-from langchain_openai import ChatOpenAI
+from gen_ai_hub.proxy.core.proxy_clients import get_proxy_client
+from gen_ai_hub.proxy.langchain.openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
 
+from config import MODEL_NAME, PROXY_CLIENT_NAME
 from mcp import ClientSession, StdioServerParameters
-from mcp.server.stdio import studio_client
 
 load_dotenv()
 
+proxy_client = get_proxy_client(PROXY_CLIENT_NAME)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/I756693/Documents/mcp_project/mcp/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from mcp!")
 
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
