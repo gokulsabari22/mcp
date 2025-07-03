764dd2c29ca7c33b2fa1ed3169d206ed9f6d325d
gokul <gokul.gowri.shankar.krishnan@sap.com>
2025-07-03 13:59:37 +0530

    Enhance main.py and update dependencies
    - Added imports for MCP tools, ChatOpenAI, and langgraph in main.py
    - Updated pyproject.toml to include isort as a dependency
    - Modified uv.lock to reflect the addition of isort version 6.0.1
    - Added a blank line in weather_server.py for improved readability

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import asyncio
 import os
+
 from dotenv import load_dotenv
+from langchain_mcp_adapters import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.server.stdio import studio_client
 
 load_dotenv()
 
-print(os.getenv("OPENAI_API_KEY"))
 
 async def main():
     print("Hello from mcp!")
